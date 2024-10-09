e6789912a1406bb14fa8b54288cc4b56fcdf417f
Belbiaro TOUTA <trichardini@yahoo.fr>
2024-10-09 01:33:21 +0000

    Modification du programme zcasino.

--- zcasino.py
+++ zcasino.py
@@ -2,26 +2,29 @@ from random import randint
 from math import ceil
 
 bille = randint(0, 49)
-somme_mise = int(input("Saisissez le montant à miser: "))
+budget = 2000
 gain = 0
-numero_choisi = int(input("Montrer votre numéro: "))
 
-if 0 <= numero_choisi <= 49:
-    print("La valeur saisie est valide")
-    if numero_choisi == bille:
-        gain = somme_mise * 3
-        somme_mise = somme_mise + gain
-        print(f"Vous avez gagné et la somme gagné est: {gain} et votre nouveau solde est: {somme_mise}")
-    elif numero_choisi != bille and (numero_choisi % 2 == 0 or numero_choisi % 2 != 0):
-        gain = somme_mise / 2
-        gain = ceil(gain)
-        somme_mise = somme_mise + gain
-        print(f"Vous avez la moitié de votre mise et la somme est: {gain} et votre nouveau solde est: {somme_mise}")
-    else:
-        gain = gain
-        print("Vous avez perdu !")
-else:
-    print("La valeur saisie est invalide, veuillez réessayer.")
+try:
+    numero_choisi = int(input("Montrer votre numéro: "))
+    somme_mise = int(input("Saisissez le montant à miser: "))
+    if 0 <= numero_choisi <= 49:
+        if numero_choisi == bille:
+            gain = somme_mise * 3
+            budget += gain
+            print(f"Vous avez le bon numéro et vous avez obtenu un gain de {gain}")
+            print(f"Votre budget actualisé est de: {budget}")
+        elif numero_choisi % 2 == 0 or numero_choisi % 2 != 0:
+            gain = ceil(somme_mise * 0.5)
+            budget += gain
+            print(f"Vous avez le bon numéro et vous avez obtenu un gain de {gain}")
+            print(f"Votre budget actualisé est de: {budget}")
+        elif budget < 0 or somme_mise > budget:
+            print("Vous n'avez plus de fond, partie terminée.")
+    raise ValueError
+except ValueError:
+        print("Le numéro est invalide, veuillez saisir un numéro compris entre 0 et 49.")
+
 
 
 
